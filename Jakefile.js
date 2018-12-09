23efa4381aae6aa9becdfc0ad2559eff1a21760f
Sendhuraan NKK <sendhuraan967@gmail.com>
2018-12-10 01:48:35 +0530

    Modified Jakefile to automatically run all the tasks to serve

--- Jakefile.js
+++ Jakefile.js
@@ -3,251 +3,307 @@
 
 (function() {
 
-	// Uncomment and modify the following list to cause the build to fail unless these browsers are tested.
-	var REQUIRED_BROWSERS = [
-//		'IE 8.0.0 (Windows 7)',
-//		'IE 9.0.0 (Windows 7)',
-//		'Firefox 31.0.0 (Mac OS X 10.8)',
-//		'Chrome 37.0.2062 (Mac OS X 10.8.5)',
-//		'Safari 6.1.6 (Mac OS X 10.8.5)',
-//		'Mobile Safari 7.0.0 (iOS 7.1)'
-	];
+  // Uncomment and modify the following list to cause the build to fail unless these browsers are tested.
+  var REQUIRED_BROWSERS = [
+//    'IE 8.0.0 (Windows 7)',
+//    'IE 9.0.0 (Windows 7)',
+//    'Firefox 31.0.0 (Mac OS X 10.8)',
+//    'Chrome 37.0.2062 (Mac OS X 10.8.5)',
+//    'Safari 6.1.6 (Mac OS X 10.8.5)',
+//    'Mobile Safari 7.0.0 (iOS 7.1)'
+  ];
 
-	var shell = require('shelljs');
-	var path = require('path');
-	var fs = require('fs');
+  var shell = require('shelljs');
+  var path = require('path');
+  var fs = require('fs');
 
 
-	var GENERATED_DIR = 'generated';
-	var JSX_DIR = GENERATED_DIR + '/jsx';
-	var BROWSERIFY_DIR = GENERATED_DIR + '/browserify';
-	var COLLATED_CLIENT_DIR = GENERATED_DIR + '/client';
-	var DEPLOY_DIR = GENERATED_DIR + '/deploy';
+  var GENERATED_DIR = 'generated';
+  var JSX_DIR = GENERATED_DIR + '/jsx';
+  var BROWSERIFY_DIR = GENERATED_DIR + '/browserify';
+  var COLLATED_CLIENT_DIR = GENERATED_DIR + '/client';
+  var DEPLOY_DIR = GENERATED_DIR + '/deploy';
 
-	var COLLECTION_DIR = 'src/collection/';
+  var COLLECTION_DIR = 'src/collection/';
 
-	var CLIENT_DIR = 'src/collection';
-	var VENDOR_DIR = 'src/utilities';
+  var CLIENT_DIR = 'src/collection';
+  var VENDOR_DIR = 'src/utilities';
 
-	directory(GENERATED_DIR);
-	directory(BROWSERIFY_DIR);
-	directory(COLLATED_CLIENT_DIR);
-	directory(DEPLOY_DIR);
+  directory(GENERATED_DIR);
+  directory(BROWSERIFY_DIR);
+  directory(COLLATED_CLIENT_DIR);
+  directory(DEPLOY_DIR);
 
-	desc('Default Task');
-	task('default', ['lint'], function() {
-		console.log('Build OK');
-	});
+  desc('Default Task');
+  task('default', ['lint'], function() {
+    console.log('Build OK');
+  });
 
-	desc('Lint All JS Files');
-	task('lint', function() {
-		
-		var lintConfig = require('./build/config/eslint.config.js');
-		var lintRunner = require('./build/utilities/lint-runner.js');
+  desc('Lint All JS Files');
+  task('lint', function() {
+    
+    var lintConfig = require('./build/config/eslint.config.js');
+    var lintRunner = require('./build/utilities/lint-runner.js');
 
-		var listES5 = new jake.FileList();
-		listES5.include('**/*.js');
-		if (fs.existsSync('node_modules')) {
-			listES5.exclude('node_modules/*');
-		}
-		if (fs.existsSync(GENERATED_DIR)) {
-			listES5.exclude(GENERATED_DIR + '/*');
-		}
-		
-		var listES6 = new jake.FileList();
-		listES6.include('src/collection/**/*.jsx');
+    var listES5 = new jake.FileList();
+    listES5.include('**/*.js');
+    if (fs.existsSync('node_modules')) {
+      listES5.exclude('node_modules/*');
+    }
+    if (fs.existsSync(GENERATED_DIR)) {
+      listES5.exclude(GENERATED_DIR + '/*');
+    }
+    
+    var listES6 = new jake.FileList();
+    listES6.include('src/collection/**/*.jsx');
 
-		var lintES5 = lintRunner.validateFiles(listES5.toArray(), lintConfig.es5Options);
-		var lintES6 = lintRunner.validateFiles(listES6.toArray(), lintConfig.es6Options);
+    var lintES5 = lintRunner.validateFiles(listES5.toArray(), lintConfig.es5Options);
+    var lintES6 = lintRunner.validateFiles(listES6.toArray(), lintConfig.es6Options);
 
-		if(!lintES5 || !lintES6) {
-			fail('Lint Failed');
-		}
+    if(!lintES5 || !lintES6) {
+      fail('Lint Failed');
+    }
 
-	}, { async: true });
+  }, { async: true });
 
-	namespace('browserTests', function() {
+  namespace('browserTests', function() {
 
-		var KarmaServer = require('karma').Server;
+    var KarmaServer = require('karma').Server;
 
-		var cfg = require('karma').config;
-		var path = require('path');
+    var cfg = require('karma').config;
+    var path = require('path');
 
-		var karmaConfig = cfg.parseConfig(path.resolve('./build/config/karma.config.js'));
+    desc('Start Karma Server');
+    task('start', function(sourceDirName) {
 
-		desc('Start Karma Server');
-		task('start', function() {
+      var overrideConfig = {
+        files: [
+            'src/collection/'+sourceDirName+'/generated/client/**/*.js'
+          ],
+        preprocessors: {}
+      };
 
-			var serverInstance = new KarmaServer(karmaConfig, function(exitCode) {
-				console.log('Karma has exited with ' + exitCode);
-				process.exit(exitCode);
-			});
+      overrideConfig.preprocessors['src/collection/'+sourceDirName+'/generated/client/**/*.js'] = ['browserify'];
 
-			serverInstance.start();
-			process.stdout.write('Capture the expected browsers');
+      var karmaConfig = cfg.parseConfig(path.resolve('./build/config/karma.config.js'), overrideConfig);
 
-		});
+      var serverInstance = new KarmaServer(karmaConfig, function(exitCode) {
+        console.log('Karma has exited with ' + exitCode);
+        process.exit(exitCode);
+      });
 
-		desc('Client Tests');
-		task('run', ['transpile'], function() {
+      serverInstance.start();
+      process.stdout.write('Capture the expected browsers');
 
-			var runner = require('karma').runner;
-			runner.run(karmaConfig, function(exitCode) {
-				console.log('Karma has exited with ' + exitCode);
-				process.exit(exitCode);
-			});
+    });
 
-		});
+    desc('Client Tests');
+    task('run', function(sourceDirName) {
 
-	});
+      var overrideConfig = {
+        files: [
+            'src/collection/'+sourceDirName+'/generated/client/**/*.js'
+          ],
+        preprocessors: {}
+      };
 
-	desc('Start HTTP server for manual testing');
-	task('serve', function(sourceDirName) {
+      overrideConfig.preprocessors['src/collection/'+sourceDirName+'/generated/client/**/*.js'] = ['browserify'];
 
-		var dependentTask = jake.Task['build:unit'];
-		dependentTask.invoke.apply(dependentTask, [sourceDirName]);
+      var karmaConfig = cfg.parseConfig(path.resolve('./build/config/karma.config.js'), overrideConfig);
 
-		var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
-		var DEPLOY_DIR = SOURCE_DIR + '/deploy';
+      var runner = require('karma').runner;
+      runner.run(karmaConfig, function(exitCode) {
+        console.log('Karma has exited with ' + exitCode);
+        process.exit(exitCode);
+      });
 
-		var finalhandler = require('finalhandler');
-		var http = require('http');
-		var serveStatic = require('serve-static');
+    });
 
-		var HOST = 'localhost';
-		var PORT = 3000;
-		
-		var serve = serveStatic(DEPLOY_DIR, {'index': ['index.html', 'index.htm']});
-		
-		var server = http.createServer(function onRequest (req, res) {
-			serve(req, res, finalhandler(req, res));
-		});
-		
-		server.listen(PORT, HOST, function(error) {
-			if(error) {
-				console.log(error);
-			}
-			else {
-				console.log('Server Listening on http://' + HOST + ':' + PORT);
-			}
-			
-		});
-	}, {async: true});
+  });
 
-	desc('Create deployable client files');
-	namespace('build', function() {
+  desc('Start HTTP server for manual testing');
+  task('serve', function(sourceDirName) {
 
-		task('all', function() {
-			process.stdout.write('Building deploy dir for all folders ~');
-		});
+        var dependentTask = jake.Task['build:unit'];
+        
+        dependentTask.addListener('complete', function() {
 
-		task('unit', function(sourceDirName) {
+          console.log('serving...')
 
-			if(!sourceDirName) {
-				fail('Specify folder name for building - build:unit[folderName]');
-			}
-			else {
-				console.log('Building deploy dir for unit folder ~');
+          var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
+          var DEPLOY_DIR = SOURCE_DIR + '/deploy';
 
-				var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
-				var CLIENT_DIR = SOURCE_DIR + '/client';
-				var GENERATED_DIR = SOURCE_DIR + '/generated';
-				var BROWSERIFY_DIR = GENERATED_DIR + '/bundle';
-				var DEPLOY_DIR = SOURCE_DIR + '/deploy';
+          var finalhandler = require('finalhandler');
+          var http = require('http');
+          var serveStatic = require('serve-static');
 
-				shell.rm('-rf', DEPLOY_DIR + '/*');
-				shell.mkdir('-p', DEPLOY_DIR);
+          var HOST = 'localhost';
+          var PORT = 3000;
+          
+          var serve = serveStatic(DEPLOY_DIR, {'index': ['index.html', 'index.htm']});
+          
+          var server = http.createServer(function onRequest (req, res) {
+            serve(req, res, finalhandler(req, res));
+          });
+          
+          server.listen(PORT, HOST, function(error) {
+            if(error) {
+              console.log(error);
+            }
+            else {
+              console.log('Server Listening on http://' + HOST + ':' + PORT);
+            }
+            
+          });
 
-				shell.cp('-R',
-						CLIENT_DIR + '/*.html',
-						CLIENT_DIR + '/*.css',
-						BROWSERIFY_DIR + '/*.js',
-					DEPLOY_DIR
-				);
+        });
 
-				return sourceDirName;
-			}
+        dependentTask.invoke.apply(dependentTask, [sourceDirName]);
 
-		});
-		
-		
-	});
+  }, { async: true });
 
-	namespace('bundle', function() {
-		
-		task('all', function() {
-			process.stdout.write('Bundling all folders ~');
-		});
+  desc('Create deployable client files');
+  namespace('build', function() {
 
-		task('unit', function(sourceDirName) {
+    task('all', function() {
+      process.stdout.write('Building deploy dir for all folders ~');
+    });
 
-			if(!sourceDirName) {
-				fail('Specify folder name for bundling - bundle:unit[folderName]');
-			}
-			else {
-				console.log('Bundling unit folder ~');
+    task('unit', function(sourceDirName) {
 
-				var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
-				var GENERATED_DIR = SOURCE_DIR + '/generated';
-				var COLLATED_CLIENT_DIR = GENERATED_DIR + '/client';
-				var BROWSERIFY_DIR = GENERATED_DIR + '/bundle';
+      if(!sourceDirName) {
+        fail('Specify folder name for building - build:unit[folderName]');
+      }
+      else {
 
-				shell.rm('-rf', BROWSERIFY_DIR + '/*');
-				shell.mkdir('-p', BROWSERIFY_DIR);
+        var dependentTask = jake.Task['bundle:unit'];
+        
+        dependentTask.addListener('complete', function() {
 
-				var browserifyRunner = require('./build/utilities/browserify-runner.js');
+          console.log('Building deploy dir for unit folder ~');
 
-				browserifyRunner.bundle(COLLATED_CLIENT_DIR + '/app.js', BROWSERIFY_DIR + '/bundle.js', complete, fail);
-			}
+          var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
+          var CLIENT_DIR = SOURCE_DIR + '/client';
+          var GENERATED_DIR = SOURCE_DIR + '/generated';
+          var BROWSERIFY_DIR = GENERATED_DIR + '/bundle';
+          var DEPLOY_DIR = SOURCE_DIR + '/deploy';
 
-		});
-		
-	}, { async: true });
+          shell.rm('-rf', DEPLOY_DIR + '/*');
+          shell.mkdir('-p', DEPLOY_DIR);
 
-	namespace('transpile', function() {
+          shell.cp('-R',
+              CLIENT_DIR + '/*.html',
+              CLIENT_DIR + '/*.css',
+              BROWSERIFY_DIR + '/*.js',
+            DEPLOY_DIR
+          );
 
-		task('all', function() {
-			process.stdout.write('Transpiling all folders ');
-		});
+          complete();
 
-		task('unit', function(sourceDirName) {
+          return sourceDirName;
 
-			if(!sourceDirName) {
-				fail('Specify folder name for transpiling - transpile:unit[folderName]');
-			}
-			else {
-				process.stdout.write('Transpiling unit folder ');
+        });
 
-				var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
-				var GENERATED_DIR = SOURCE_DIR + '/generated';
-				var DEPLOY_DIR = SOURCE_DIR + '/deploy';
+        dependentTask.invoke.apply(dependentTask, [sourceDirName]);
+        
+      }
 
-				shell.rm('-rf', GENERATED_DIR + '/*');
-				shell.mkdir('-p', GENERATED_DIR);
+    }, { async: true });
+    
+    
+  });
 
-				var babelConfig = require('./build/config/babel.config.js');
-				var babelRunner = require('./build/utilities/babel-runner.js');
+  namespace('bundle', function() {
+    
+    task('all', function() {
+      process.stdout.write('Bundling all folders ~');
+    });
 
-				var transpileFileList = new jake.FileList();
-				transpileFileList.include(SOURCE_DIR + '/**/*.js');
-				transpileFileList.include(SOURCE_DIR + '/**/*.jsx');
+    task('unit', function(sourceDirName) {
 
-				var compileOptions = {
-					presets: ['@babel/preset-react', '@babel/preset-env']
-				};
+      if(!sourceDirName) {
+        fail('Specify folder name for bundling - bundle:unit[folderName]');
+      }
+      else {
 
-				var transpileStatus = babelRunner.transformFiles(SOURCE_DIR, transpileFileList.toArray(), GENERATED_DIR, babelConfig);
-				if (!transpileStatus) fail('Transpilation failed');
+        var dependentTask = jake.Task['transpile:unit'];
 
-			}
+        dependentTask.addListener('complete', function() {
 
-		});
+          console.log('Bundling unit folder ~');
 
-	});
+          var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
+          var GENERATED_DIR = SOURCE_DIR + '/generated';
+          var COLLATED_CLIENT_DIR = GENERATED_DIR + '/client';
+          var BROWSERIFY_DIR = GENERATED_DIR + '/bundle';
+
+          shell.rm('-rf', BROWSERIFY_DIR + '/*');
+          shell.mkdir('-p', BROWSERIFY_DIR);
+
+          var browserifyRunner = require('./build/utilities/browserify-runner.js');
+
+          browserifyRunner.bundle(COLLATED_CLIENT_DIR + '/app.js', BROWSERIFY_DIR + '/bundle.js', bundleComplete, fail);
+
+          function bundleComplete() {
+            complete();
+            console.log('Bundle End');
+          }
+
+          return sourceDirName;
+
+
+        });
+
+        dependentTask.invoke.apply(dependentTask, [sourceDirName]);
+
+        
+      }
+
+    }, { async: true });
+    
+  });
+
+  namespace('transpile', function() {
+
+    task('all', function() {
+      process.stdout.write('Transpiling all folders ');
+    });
+
+    task('unit', function(sourceDirName) {
+
+      if(!sourceDirName) {
+        fail('Specify folder name for transpiling - transpile:unit[folderName]');
+      }
+      else {
+        process.stdout.write('Transpiling unit folder ');
+
+        var SOURCE_DIR = COLLECTION_DIR + sourceDirName;
+        var GENERATED_DIR = SOURCE_DIR + '/generated';
+        var DEPLOY_DIR = SOURCE_DIR + '/deploy';
+
+        shell.rm('-rf', GENERATED_DIR + '/*');
+        shell.mkdir('-p', GENERATED_DIR);
+
+        var babelConfig = require('./build/config/babel.config.js');
+        var babelRunner = require('./build/utilities/babel-runner.js');
+
+        var transpileFileList = new jake.FileList();
+        transpileFileList.include(SOURCE_DIR + '/**/*.js');
+        transpileFileList.include(SOURCE_DIR + '/**/*.jsx');
+
+        var compileOptions = {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        };
+
+        var transpileStatus = babelRunner.transformFiles(SOURCE_DIR, transpileFileList.toArray(), GENERATED_DIR, babelConfig);
+        if (!transpileStatus) fail('Transpilation failed');
+
+        complete();
+
+      }
+
+    }, { async: true });
+
+  });
 
 })();
-
-
-
-
-
