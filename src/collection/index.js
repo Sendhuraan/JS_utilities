880bfcb2da19d862142f324dd6268c23a666e70a
Sendhuraan NKK <snkannan@paypal.com>
2019-01-24 19:34:45 +0530

    Added publicAPI

--- src/collection/index.js
+++ src/collection/index.js
@@ -1,8 +1,12 @@
 (function() {
 
+	var BitUtils = require('./bit-utilities');
+	var MathUtils = require('./math-utilities');
 	var SortUtils = require('./sort-utilities');
 
 	var publicAPI = {
+		BitUtils: BitUtils,
+		MathUtils: MathUtils,
 		SortUtils: SortUtils
 	}
 
